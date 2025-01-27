1d79583c219ca7fabe8a0bdbb2ab688bdeb90ef8
Paul Sonnentag <paul.sonnentag@googlemail.com>
2025-01-27 13:51:46 +0100

    allow to tweak card face

--- cards/card.gd
+++ cards/card.gd
@@ -1,15 +1,32 @@
+@tool
+
 class_name Card
 extends Node2D
 
 @onready var draggable: Draggable = %Draggable
 @export var entity_id: String
 
+@onready var image_sprite: Sprite2D = %FaceSprite
+@export var image: Texture2D:
+	set(value):
+		image = value
+		if image_sprite:
+			image_sprite.texture = value
+
 func _ready() -> void:
+	if Engine.is_editor_hint():
+		return
+
+	if image:
+		image_sprite.texture = image
 
 	draggable.dropped.connect(_on_dropped)
 
 
 func _process(_delta: float) -> void:
+	if Engine.is_editor_hint():
+		return
+
 	if !draggable.is_dragging:
 		var x = Global.state.get_state_int(entity_id, "x")
 		var y = Global.state.get_state_int(entity_id, "y")
--- global.gd
+++ global.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var state: GodotProject = GodotProject.create("")
+var state: GodotProject = GodotProject.create("3PmJQKaMkfw4yY1hD1PsLnphzsXy")
 
 func _ready():
   print(state.get_doc_id())
