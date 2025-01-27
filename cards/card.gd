1cd4f6433d1852688a5e02196ff741b5c81deda8
Paul Sonnentag <paul.sonnentag@googlemail.com>
2025-01-27 14:08:50 +0100

    add private state to flip cards

--- cards/card.gd
+++ cards/card.gd
@@ -6,28 +6,37 @@ extends Node2D
 @onready var draggable: Draggable = %Draggable
 @export var entity_id: String
 
-@onready var image_sprite: Sprite2D = %FaceSprite
+@export var is_revealed = false
+
+@onready var face_sprite: Sprite2D = %FaceSprite
+@onready var back_sprite: Sprite2D = %BackSprite
+
 @export var image: Texture2D:
 	set(value):
 		image = value
-		if image_sprite:
-			image_sprite.texture = value
+		if face_sprite:
+			face_sprite.texture = value
 
 func _ready() -> void:
 	if Engine.is_editor_hint():
 		return
 
 	if image:
-		image_sprite.texture = image
+		face_sprite.texture = image
 
 	draggable.dropped.connect(_on_dropped)
+	draggable.clicked.connect(_on_click)
 
+func _on_click() -> void:
+	is_revealed = !is_revealed
 
 func _process(_delta: float) -> void:
 	if Engine.is_editor_hint():
 		return
 
-	if !draggable.is_dragging:
+	back_sprite.visible = !is_revealed
+
+	if !draggable.is_mouse_down:
 		var x = Global.state.get_state_int(entity_id, "x")
 		var y = Global.state.get_state_int(entity_id, "y")
 
--- dnd/draggable.gd
+++ dnd/draggable.gd
@@ -3,57 +3,68 @@ extends Node2D
 
 static var currently_dragged: Node2D
 
-var is_dragging = false
+var has_moved = false;
+var is_mouse_down = false
 var is_hovered = false
 
 signal dropped(drop_target: Node2D);
+signal clicked();
 
 @export var drag_handle: Node
 
 func _input(event: InputEvent) -> void:
-  if not event.position:
-    return
+		if not event.position:
+				return
 
-  is_hovered = drag_handle.get_rect().has_point(drag_handle.to_local(event.position))
+		is_hovered = drag_handle.get_rect().has_point(drag_handle.to_local(event.position))
 
-  if Input.is_action_just_pressed("pointer") and is_hovered and currently_dragged == null:
-    currently_dragged = get_parent()
-    is_dragging = true
-    return
+		if Input.is_action_just_pressed("pointer") and is_hovered and currently_dragged == null:
+				currently_dragged = get_parent()
+				is_mouse_down = true
+				has_moved = false
+				return
 
-  if currently_dragged != get_parent():
-    return
+		if currently_dragged != get_parent():
+				return
 
-  if Input.is_action_just_released("pointer") and is_dragging:
-    is_dragging = false
-    currently_dragged = null
-    _on_drop(event.position)
-    return
+		if Input.is_action_just_released("pointer") and is_mouse_down:
+				is_mouse_down = false
+				currently_dragged = null
+				if not has_moved:
+						clicked.emit()
+				else:
+						_on_drop(event.position)
+				return
 
-  _on_drag(event.position)
+		if is_mouse_down:
+				has_moved = true
+				_on_drag(event.position)
 
 func _on_drag(point: Vector2):
-  for target in DropTarget.all_drop_targets:
-    target.is_dragging_over = target.has_point(point)
+		for target in DropTarget.all_drop_targets:
+				target.is_dragging_over = target.has_point(point)
 
 
 func _on_drop(point: Vector2):
-  var was_dropped_on_target = false
+		var was_dropped_on_target = false
 
-  for target in DropTarget.all_drop_targets:
-    target.is_dragging_over = false
+		for target in DropTarget.all_drop_targets:
+				target.is_dragging_over = false
 
-    if target.has_point(point):
-      dropped.emit(target.get_parent())
-      target.dropped.emit(get_parent())
-      was_dropped_on_target = true
-      break
+				if target.has_point(point):
+						dropped.emit(target.get_parent())
+						target.dropped.emit(get_parent())
+						was_dropped_on_target = true
+						break
 
-  if !was_dropped_on_target:
-    dropped.emit(null)
+		if !was_dropped_on_target:
+				dropped.emit(null)
 
 func _process(_delta: float):
-  var parent = get_parent()
+		var parent = get_parent()
 
-  if is_dragging:
-    parent.global_position = parent.global_position.lerp(get_global_mouse_position(), 0.5)
+		if is_mouse_down && has_moved:
+				parent.global_position = parent.global_position.lerp(get_global_mouse_position(), 0.5)
+
+func is_dragging():
+		return is_mouse_down && has_moved
